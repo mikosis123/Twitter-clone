6db7dcefcab771c040bb327153deea9f596054ba
unknown <mikosis93@gmail.com>
2023-01-18 04:47:58 -0800

    insertion of logo

---
+++ components/sidebar.js
@@ -0,0 +1,15 @@
+
+
+export default function Sidebar() {
+  return (
+    <div>
+        {/* twitter logo */}
+      <div>
+        <Image hight ="50 px" width ="40 px" src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FTwitter&psig=AOvVaw0Eriyff719jrGeDl4MBXi2&ust=1674070204787000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCKDyud6rz_wCFQAAAAAdAAAAABAH"></Image>
+      </div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
+import Sidebar from '../components/sidebar'
+
 
 
 
@@ -14,7 +14,16 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className=' size-lg text-center'>hello world</h1>
+      <main>
+{/* side bar */}
+
+<Sidebar />
+{/* feed  */}
+{/* wedjets */}
+{/* modal */}
+
+
+      </main>
       
     </>
   )
