39c7c173ed50fcabbb5cb09b7c3701a4bbd808ae
unknown <mikosis93@gmail.com>
2023-01-29 02:40:18 -0800

    side bar spacing

--- components/sidebar.js
+++ components/sidebar.js
@@ -7,13 +7,13 @@ import {BellIcon, BookmarkAltIcon, BookmarkIcon, ClipboardIcon, DotsCircleHorizo
 
 export default function Sidebar() {
   return (
-    <>
+    <div className='hidden sm:flex flex-col xl:items-start fixed h-full p-2'>
         {/* twitter logo */}
-      <div className="hoverEffect">
+      <div className="hoverEffect ">
         <Image height ="50" width ="50" src = {image}></Image>
       </div>
        {/* side bar menu items */}
-       <div className=''>
+       <div className='mt-4 mb-2.4 xl:item-start'>
        <Sidebarmenu text="Home" Icon={HomeIcon} />
        <Sidebarmenu text="explore" Icon={HashtagIcon} />
        <Sidebarmenu text="Notifications" Icon={BellIcon} />
@@ -26,16 +26,16 @@ export default function Sidebar() {
       </div>
       {/* button */}
       <div>
-        <button className='bg-blue-400 text-white rounded-full h-12 w-56 font-bold shadow-md hover:brightness-95 text-lg hidden lg:inline'>tweet</button>
+        <button className='bg-blue-400 text-white rounded-full h-12 w-56 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline'>tweet</button>
       </div>
-      <div className='hoverEffect text-gray-700'>
-        <Image className='rounded-full' height ="100" width ="100" src = {isk}></Image>
-        <div>
-          <h4>mikiyas sisay</h4>
-          <p>@ miko</p>
+      <div className='hoverEffect flex item-center justify-center text-gray-700 xl:justify-start mt-auto'>
+        <Image className='rounded-full h-10 w-10 mr-2 mr-x-auto' height ="100" width ="100" src = {isk}></Image>
+        <div className='hidden xl:inline'>
+          <h4 className='text-gray-600'>mikiyas sisay</h4>
+          <p className='text-gray-500'>@ miko</p>
         </div>
-        <DotsHorizontalIcon className=' h-6' />
+        <DotsHorizontalIcon className=' h-6 ml-5hidden xl:inline' />
       </div>
-    </>
+    </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
+      <main className='flex min-h-screen max-w-7xl mx-auto'>
 {/* side bar */}
 
 <Sidebar />
