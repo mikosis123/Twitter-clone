6a3f9e65b42e5873d0999858875d1844cf68d9fd
mikosis123 <mikosis93@gmail.com>
2023-12-24 05:44:16 -0800

    rusers return as jsx

--- components/Wedjet.js
+++ components/Wedjet.js
@@ -2,7 +2,7 @@ import { SearchIcon } from "@heroicons/react/outline";
 import { useState, setState } from "react";
 import News from "./News";
 
-export default function Wedjet({ newresult }) {
+export default function Wedjet({ newresult, randomusers }) {
   const [Artnum, setArtnum] = useState(3);
   return (
     <div className="xl:w-[600px] hidden lg:inline ml-8 space-y-5  ">
@@ -28,8 +28,17 @@ export default function Wedjet({ newresult }) {
           show more
         </button>
       </div>
-      <div>
+      <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[90%] xl:w-[75%]">
         <h4 className="font-bold text-xl px-4">Who to follow</h4>
+        {randomusers.map((rusers) => {
+          return (
+            <div key={rusers.login.username}>
+              <h4 key={rusers.login.username} className=" ">
+                {rusers.login.username}
+              </h4>
+            </div>
+          );
+        })}
         <button className="text-blue-300 pl-4 pb-3 hover:text-blue-400">
           Show more
         </button>
