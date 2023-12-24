6889d404dda18753c48f047e817bba812f96cae3
mikosis123 <mikosis93@gmail.com>
2023-12-24 06:04:45 -0800

    adding see more functionality

--- components/Wedjet.js
+++ components/Wedjet.js
@@ -4,6 +4,7 @@ import News from "./News";
 
 export default function Wedjet({ newresult, randomusers }) {
   const [Artnum, setArtnum] = useState(3);
+  const [Numrusers, setNumrusers] = useState(3);
   return (
     <div className="xl:w-[600px] hidden lg:inline ml-8 space-y-5  ">
       <div className="w-[90%] xl:w-[75%] sticky bg-white py-1.5 z-50">
@@ -30,16 +31,40 @@ export default function Wedjet({ newresult, randomusers }) {
       </div>
       <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[90%] xl:w-[75%]">
         <h4 className="font-bold text-xl px-4">Who to follow</h4>
-        {randomusers.map((rusers) => {
+        {randomusers.slice(0, Numrusers).map((rusers) => {
           return (
             <div key={rusers.login.username}>
-              <h4 key={rusers.login.username} className=" ">
-                {rusers.login.username}
-              </h4>
+              <div
+                key={rusers.login.username}
+                className="flex items-center px-4 py-2  cursor-pointer hover:bg-gray-200 transition duration-500 ease-out"
+              >
+                <img
+                  className="rounded-full"
+                  width="40"
+                  src={rusers.picture.thumbnail}
+                  alt=""
+                />
+                <div className="truncate ml-4 leading-5">
+                  <h4 className="font-bold hover:underline text-[14px] truncate">
+                    {rusers.login.username}
+                  </h4>
+                  <h5 className="text-[13px] text-gray-500 truncate">
+                    {rusers.name.first + " " + rusers.name.last}
+                  </h5>
+                </div>
+                <button className="ml-auto bg-black text-white rounded-full text-sm px-3.5 py-1.5 font-bold">
+                  Follow
+                </button>
+              </div>
             </div>
           );
         })}
-        <button className="text-blue-300 pl-4 pb-3 hover:text-blue-400">
+        <button
+          onClick={() => {
+            setNumrusers(Numrusers + 5);
+          }}
+          className="text-blue-300 pl-4 pb-3 hover:text-blue-400"
+        >
           Show more
         </button>
       </div>
