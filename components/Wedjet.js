eb7e9618446175c3da85e08a47123384e385b5af
mikosis123 <mikosis93@gmail.com>
2023-12-24 04:01:42 -0800

    adding useState

--- components/Wedjet.js
+++ components/Wedjet.js
@@ -1,7 +1,9 @@
 import { SearchIcon } from "@heroicons/react/outline";
+import { useState } from "react";
 import News from "./News";
 
 export default function Wedjet({ newresult }) {
+  const [Artnum, setArtnum] = useState(3);
   return (
     <div className="xl:w-[600px] hidden lg:inline ml-8 space-y-5  ">
       <div className="w-[90%] xl:w-[75%] sticky bg-white py-1.5 z-50">
@@ -16,7 +18,7 @@ export default function Wedjet({ newresult }) {
       </div>
       <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[90%] xl:w-[75%]">
         <h4 className="font-bold text-xl px-4">what's happening</h4>
-        {newresult.map((article) => (
+        {newresult.slice(0, Artnum).map((article) => (
           <News title={article.title} article={article} />
         ))}
         <button className="text-blue-300 pl-4 pb-3 hover:text-blue-400">
