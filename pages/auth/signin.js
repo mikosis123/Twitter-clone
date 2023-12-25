1ffc04ff2db2cb22af6eafa4bdfd162cc020bc79
mikosis123 <mikosis93@gmail.com>
2023-12-25 14:19:52 +0300

    google page

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { getProviders } from "next-auth/react";
+import { getProviders, signIn } from "next-auth/react";
 
 function Signin({ providers }) {
   return (
@@ -9,7 +9,7 @@ function Signin({ providers }) {
         alt="twitter image inside a phone"
         className="hidden object-cover md:w-44 md:h-80 rotate-6  md:inline-flex"
       />
-      <h1>heloo sign in </h1>
+
       <div className="">
         {Object.values(providers).map((provider) => (
           <div key={provider.username} className="flex flex-col items-center">
@@ -21,7 +21,10 @@ function Signin({ providers }) {
             <p className="text-center text-sm italic my-10">
               This app is created for learning purposes
             </p>
-            <button className="bg-red-400 rounded-lg p-3 text-white hover:bg-red-500">
+            <button
+              onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+              className="bg-red-400 rounded-lg p-3 text-white hover:bg-red-500"
+            >
               Sign in with Google{console.log(provider)}
               {provider.name}
             </button>
