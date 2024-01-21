e2310a61466dcc57b9fd25a2e291ab5ef856a674
mikosis123 <mikosis93@gmail.com>
2024-01-21 09:04:11 +0300

    fire stor setup

--- components/Input.js
+++ components/Input.js
@@ -1,11 +1,24 @@
 import { EmojiHappyIcon, PhotographIcon } from "@heroicons/react/outline";
 import Image from "next/image";
 import picture from "../public/isk.jpg";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { db } from "../firebase";
 import { useSession, signOut } from "next-auth/react";
+import { useState } from "react";
 
 export default function Input() {
   const { data: session } = useSession();
   console.log(session);
+  const [Input, setInput] = useState("");
+  const sendPost = async () => {
+    const addref = await addDoc(collection(db, "posts"), {
+      id: session.user.uid,
+
+      text: Input,
+    });
+    setInput("");
+  };
+
   return (
     <>
       {session && (
@@ -22,6 +35,8 @@ export default function Input() {
               <textarea
                 className="w-full border-none focus:ring-0 text-lg placeholder-gay-700 tracking-wide min-h-[50px] text-gray-700"
                 rows={2}
+                value={Input}
+                onChange={(e) => setInput(e.target.value)}
                 placeholder="What's hapening"
               ></textarea>
             </div>
@@ -30,7 +45,11 @@ export default function Input() {
                 <PhotographIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-200" />
                 <EmojiHappyIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-200" />
               </div>
-              <button className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-medium hover:brightness-95 ">
+              <button
+                onClick={sendPost}
+                disabled={!Input.trim()}
+                className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-medium hover:brightness-95 "
+              >
                 Tweet
               </button>
             </div>
