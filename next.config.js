2250e0db589a357c99d617d043d11c3b1c5d8bbf
mikosis123 <mikosis93@gmail.com>
2024-01-21 08:10:29 +0300

    pckage addition

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXTAUTH_SECRET: "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",
+  },
 };
 
 module.exports = nextConfig;
