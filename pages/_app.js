eff924ec10fb08955cc2a7fd0a341eeecbb1e421
mikosis123 <mikosis93@gmail.com>
2024-01-21 08:11:59 +0300

    base line chage

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
+"use client";
 import "../styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+
 import { SessionProvider } from "next-auth/react";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
