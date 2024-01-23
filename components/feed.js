2bbbc3a8b942fecf4c55ae13f9641c7178d47e32
mikosis123 <mikosis93@gmail.com>
2024-01-23 00:15:34 -0800

    snapshot comp

--- components/feed.js
+++ components/feed.js
@@ -1,42 +1,33 @@
 import { SparklesIcon } from "@heroicons/react/outline";
 import Input from "./Input";
 import Posts from "./post";
-
+import { useEffect, useState } from "react";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 
 export default function Feed() {
-  const posts = [
-    {
-      id:1,
-      name: "mikiyas sisay",
-      username:"miko",
-      userimage : "https://media.istockphoto.com/id/1423284665/photo/man-tourist-walking-on-the-ice-of-baikal-lake-winter-landscape-of-baikal-lake-russia.jpg?b=1&s=170667a&w=0&k=20&c=VsezULZvjg-xRsDPRdmJbNdDdEV8Y3B9c7HJ6pfkbpg=",
-      img :"https://media.istockphoto.com/id/1427249962/photo/tropical-leaves-abstract-green-leaf-texture-in-garden-nature-background.jpg?b=1&s=170667a&w=0&k=20&c=40KcV7mbAQWihuO0At8GSOTIKp-TvIoHGIhurHBeUq0=",
-      text:"waw the nature buty",
-      timestamp:"2 munites ago"
-    }, 
-    {
-      id:2,
-      name: "mikiyas sisay",
-      username:"miko",
-      userimage : "https://media.istockphoto.com/id/1423284665/photo/man-tourist-walking-on-the-ice-of-baikal-lake-winter-landscape-of-baikal-lake-russia.jpg?b=1&s=170667a&w=0&k=20&c=VsezULZvjg-xRsDPRdmJbNdDdEV8Y3B9c7HJ6pfkbpg=",
-      img :"https://media.istockphoto.com/id/1411846592/photo/mountains-in-low-dense-fog-at-morning-sunrise-beautiful-landscape-with-mountain-peaks-in-fog.jpg?b=1&s=170667a&w=0&k=20&c=DPlaHpdGDq5dfTjE6JNUGr3BTRBYTMpQI-ai12mU4Qg=",
-      text:"clouds and nature buty",
-      timestamp:"2 munites ago"
-    }
-  ]
+  const [posts, setPosts] = useState([]);
+  useEffect(
+    () =>
+      onSnapshot(
+        query(collection(db, "posts"), orderBy("timestamp", "desc")),
+        (snapshot) => {
+          setPosts(snapshot.docs);
+        }
+      ),
+    []
+  );
   return (
     <div className="xl:ml-[370px] border-l boarder-gray-200 border-r min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
- <div className="flex sticky py-2 px-3 bg-white top-0 z-50 border-b border-gray-200 justify-between">
-  <h2 className="text-lg sm:text-xl cursor-pointer font-bold">Home</h2>
-  <div className="hoverEffect ">
-    <SparklesIcon className="h-5 "/>
-  </div>
- </div>
- <Input />
- {posts.map((post)=>(
- <Posts key={post.id} post={post} />
- ))}
+      <div className="flex sticky py-2 px-3 bg-white top-0 z-50 border-b border-gray-200 justify-between">
+        <h2 className="text-lg sm:text-xl cursor-pointer font-bold">Home</h2>
+        <div className="hoverEffect ">
+          <SparklesIcon className="h-5 " />
+        </div>
+      </div>
+      <Input />
+      {posts.map((post) => (
+        <Posts key={post.id} post={post} />
+      ))}
     </div>
-    
-  )
+  );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
 "use client";
 import "../styles/globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
-
 import { SessionProvider } from "next-auth/react";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
