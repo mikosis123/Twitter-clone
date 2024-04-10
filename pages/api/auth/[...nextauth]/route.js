0d94f465f7adfdb95c4608d36459ba3609635f03
mikosis123 <mikosis93@gmail.com>
2024-04-10 13:47:30 +0300

    raute addition

---
+++ pages/api/auth/[...nextauth]/route.js
@@ -0,0 +1,39 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      httpOptions: {
+        timeout: 10000,
+      },
+    }),
+    // GitHubProvider({
+    //   clientId: GITHUB_CLIENT_ID,
+    //   clientSecret: GITHUB_CLIENT_SECRET,
+    //   httpOptions: {
+    //     timeout: 10000,
+    //   },
+    // }),
+    // ...add more providers here
+  ],
+
+  pages: {
+    signIn: "/auth/signin",
+  },
+  secret: process.env.SECRET,
+  callbacks: {
+    async session({ session, token, user }) {
+      session.user.username = session.user.name
+        .split(" ")
+        .join("")
+        .toLocaleLowerCase();
+      session.user.uid = token.sub;
+      return session;
+    },
+  },
+});
